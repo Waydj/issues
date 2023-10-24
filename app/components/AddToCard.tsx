7d69d4775762a82534e28bea609955200cb008c0
Waydj <Farhminait@gmail.com>
2023-10-24 22:12:43 +0200

    add daisyui

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const AddToCard = () => {
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => console.log("click")}>
+        Click
+      </button>
+    </div>
+  );
+};
+
+export default AddToCard;
